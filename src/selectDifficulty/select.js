1e0404104d6e8d565aac46ad516ae3f45624bdb6
Odmaral Tumenjargal <odmaraltumenjargal@Odmarals-MacBook-Air.local>
2022-06-27 13:42:01 -0700

    finished select mode

--- src/selectDifficulty/select.js
+++ src/selectDifficulty/select.js
@@ -1,3 +1,40 @@
+const box = document.getElementById("box");
+const back = document.createElement("a");
+back.href = "../index.html";
+box.append(back);
+const img = document.createElement("img");
+img.id = "img";
+img.src = "https://img.icons8.com/ios-filled/50/undefined/left2.png";
+back.append(img);
+const gameMode = document.createElement("div");
+gameMode.id = "gameMode";
+gameMode.innerText = "Select game mode: ";
+box.append(gameMode);
+const boxOne = document.createElement("div");
+boxOne.id = "boxOne";
+box.append(boxOne);
+const easy = document.createElement("a");
+easy.id = "easy";
+easy.href = "../EasyMode/easy.html";
+easy.innerText = "Easy";
+boxOne.append(easy)
+const boxTwo = document.createElement("div");
+boxTwo.id = "boxTwo";
+box.append(boxTwo);
+const normal = document.createElement("a");
+normal.id = "normal";
+normal.href = "../gamePage/game.html";
+normal.innerText = "Normal";
+boxOne.append(normal)
+const boxThree = document.createElement("div");
+boxThree.id = "boxThree";
+box.append(boxThree);
+const hard = document.createElement("a");
+hard.id = "hard";
+hard.href = "../HardMode/hard.html";
+hard.innerText = "Hard";
+boxThree.append(easy)
+
 const normal = document.getElementById("normal");
 const easy = document.getElementById("easy");
 const hard = document.getElementById("hard");
